9f3ac9eabe3c3d23a1e819caae374f240bd41790
taniauser-25 <taniajucse25@gmail.com>
2019-05-03 00:16:39 -0700

    second commit

--- app/src/main/java/com/example/jutransport/HomeActivity.java
+++ app/src/main/java/com/example/jutransport/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.jutransport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void login(View view) {
+        Intent intent=new Intent(HomeActivity.this,loginActivity.class);
+        startActivity(intent);
+    }
+
+    public void register(View view) {
+        Intent intent=new Intent(HomeActivity.this,registerActivity.class);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        Intent intent=new Intent(HomeActivity.this,logoutActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/jutransport/registerActivity.java
@@ -0,0 +1,13 @@
+package com.example.jutransport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
